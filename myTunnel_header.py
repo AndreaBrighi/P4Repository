f20128a60a39b29fae400ceaaa423c990fa6c6c0
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-06-07 17:39:48 +0200

    update mytunnel header

--- myTunnel_header.py
+++ myTunnel_header.py
@@ -10,9 +10,9 @@ class MyTunnel(Packet):
     fields_desc = [
         ShortField("pid", 0),
         ShortField("dst_id", 0),
-        ShortField("IP_MAL", 0),
-        ShortField("TIME", 0),
-        ShortField("FLAG", 0)
+        BitField("IP_MAL", 0, size=32),
+        BitField("TIME", 0, size=48),
+        BitField("FLAG", 0, size = 1)
     ]
     def mysummary(self):
         return self.sprintf("pid=%pid%, dst_id=%dst_id%")
